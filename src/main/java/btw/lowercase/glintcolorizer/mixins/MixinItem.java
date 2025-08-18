c67ced7ed86b00d9b3d8f6f0e21abffb6f64b1f1
lowercasebtw <jejojora2@gmail.com>
2025-08-17 21:27:49 -0400

    improve glint detection in MixinItem for potions

--- src/main/java/btw/lowercase/glintcolorizer/mixins/MixinItem.java
+++ src/main/java/btw/lowercase/glintcolorizer/mixins/MixinItem.java
@@ -3,16 +3,26 @@ package btw.lowercase.glintcolorizer.mixins;
 import btw.lowercase.glintcolorizer.config.GlintColorizerConfig;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionContents;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-@Mixin(Item.class)
+@Mixin(value = Item.class, priority = 999999)
 public abstract class MixinItem {
     @WrapOperation(method = "isFoil", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEnchanted()Z"))
     private boolean glintcolorizer$enablePotionGlint(ItemStack instance, Operation<Boolean> original) {
-        return (instance.getItem() instanceof PotionItem && GlintColorizerConfig.instance().shinyPots.enabled) || original.call(instance);
+        boolean hasGlint = original.call(instance);
+        if (GlintColorizerConfig.instance().shinyPots.enabled && instance.getItem() instanceof PotionItem && !hasGlint) {
+            PotionContents potionContents = instance.get(DataComponents.POTION_CONTENTS);
+            if (potionContents != null) {
+                hasGlint = potionContents.hasEffects();
+            }
+        }
+
+        return hasGlint;
     }
 }
